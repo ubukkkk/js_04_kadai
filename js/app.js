818e2f776aa8a75c984dee5f965e20d0f6dd2d32
ubukkkk <80142170+ubukkkk@users.noreply.github.com>
2021-03-12 03:13:00 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,28 +21,6 @@ $(function(){
     mon = today.getMonth() + 1;
 });
 
-// 画像投稿(未完成)
-// document.getElementById('upload').addEventListener('click', function() {
-//     files = document.getElementById('file').files;
-//     image = files[0];
-//     if(files[0].type.indexOf('image') >= 0) {
-//     ref = firebase.storage().ref().child(image.name);
-//     ref.put(image).then(function(snapshot) {
-//     ref.getDownloadURL().then(function(url){
-//     alert('画像をアップロードしました');
-//     document.getElementById('image').src = url;
-//     });
-//     });
-//     } else {
-//     alert('アップロードできるのはjpeg、png、gifの画像だけです');
-//     }
-//     });
-//     document.getElementById('delete').addEventListener('click', function() {
-//     ref.delete().then(function(){
-//     alert('画像を削除しました');
-//     });
-//     });
-
 //アイコン使用
 var d = 0;
 const img = ["cure_blossom.png", "cure_happy.png", "cure_marin.png","cure_beauty.png", "cure_sunshine.png", "cure_peace.png"]
@@ -84,4 +62,27 @@ newPostRef.on("child_added", function(data){
     str += '<br><div id="text" style="color:#8276ff">'+v.text+'</div>';
     str += '<br><br><div id="day">'+v.date+'</div><hr>';
     $("#output").append(str);
+    
+
+// 画像投稿(未完成)
+// document.getElementById('upload').addEventListener('click', function() {
+//     files = document.getElementById('file').files;
+//     image = files[0];
+//     if(files[0].type.indexOf('image') >= 0) {
+//     ref = firebase.storage().ref().child(image.name);
+//     ref.put(image).then(function(snapshot) {
+//     ref.getDownloadURL().then(function(url){
+//     alert('画像をアップロードしました');
+//     document.getElementById('image').src = url;
+//     });
+//     });
+//     } else {
+//     alert('アップロードできるのはjpeg、png、gifの画像だけです');
+//     }
+//     });
+//     document.getElementById('delete').addEventListener('click', function() {
+//     ref.delete().then(function(){
+//     alert('画像を削除しました');
+//     });
+//     });
 });
